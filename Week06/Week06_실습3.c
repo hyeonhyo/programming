06a4e3928ca316baf6fbf61f575ae452b24cfa36
hyeonhyo <hy_x_two@sookmyung.ac.kr>
2024-10-14 16:18:06 +0900

    Week06_실습3

---
+++ Week06/Week06_실습3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int num =0;
+	char c;
+	
+	while ((c=getchar())!='\n')
+	if (c >= '0' && c<='9')
+	num+=1;
+	printf("the number of digits is %d", num);
+	return 0;
+}
