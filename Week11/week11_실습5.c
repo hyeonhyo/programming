d42e24e61b5b52f8fbdfe8d46e2fd6a38fdfd8f5
hyeonhyo <hy_x_two@sookmyung.ac.kr>
2024-11-22 12:54:48 +0900

    Week11_실습5

--- Week11/week11_실습5.c
+++ Week11/week11_실습5.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    FILE *fp;
-    char c[3];  
+int main() {
+	
+	FILE *fp;
+    char w;  
     int i;
+    char c;
 
   
     for (i = 0; i < 3; i++){
         printf("input a word: "); 
-        scanf("%s", &c[i]);  
+        scanf("%s", &w);  
 	}
 
     fp = fopen("sample.txt", "w");
 
 
     for (i = 0; i < 3; i++) {
-        fprintf(fp, "%s", c[i]);  
+        fprintf(fp, "%s", w);  
     }
 
     fclose(fp);  
+    
+    fp = fopen("sample.txt", "r");
+   
+    while ((c = fgetc(fp)) != EOF) {
+        putchar(c);  
+    }
 
+    fclose(fp);
     return 0;
 }
-
 
