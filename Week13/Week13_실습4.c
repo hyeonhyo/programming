d3ffedaa41f756c567a577adc5c2c1acebd706f9
hyeonhyo <hy_x_two@sookmyung.ac.kr>
2024-11-27 23:08:57 +0900

    Week13_실습4

---
+++ Week13/Week13_실습4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct point {
+    int x;
+    int y;
+};
+
+
+int getArea(struct point p1, struct point p2);
+
+int main(int argc, char *argv[]) {
+    struct point p1, p2;
+    int area;
+
+    
+    printf("Input the coordinate p1 (x,y): ");
+    scanf("%d %d", &p1.x, &p1.y);
+    printf("Input the coordinate p2 (x,y): ");
+    scanf("%d %d", &p2.x, &p2.y);
+
+    
+    area = getArea(p1, p2);
+    printf("Area: %d\n", area);
+}
+
+int getArea(struct point p1, struct point p2){
+	int area = ((p2.x-p1.x)*(p2.y-p1.y));
+	return area;
+}
+	
+
